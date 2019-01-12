652749b60d3026176e1698953364f7fd69fefcf6
Bhushan Kalode <bhushankalode12@gmail.com>
2019-01-12 23:35:41 +0530

    hotel_order_nestedHash.rb modified with some indent

--- hotel_order_nestedHash.rb
+++ hotel_order_nestedHash.rb
@@ -1,28 +1,26 @@
 hotel_menu = {
-                Box8: { 'Rajma'=> 30, 'Vada Pav'=> 50, 'Dal'=> 20 },
-                Locavore: { 'Dal'=> 10, 'Vada Pav'=> 30, 'Sabji'=> 40 },
-                Flavours: { 'Ice Cream'=> 10, 'Burger'=> 30, 'Vada Pav'=> 30 }
+                Box8: {'Rajma'=> 30, 'Vada Pav'=> 50, 'Dal'=> 20},
+                Locavore: {'Dal'=> 10, 'Vada Pav'=> 30, 'Sabji'=> 40},
+                Flavours: {'Ice Cream'=> 10, 'Burger'=> 30, 'Vada Pav'=> 30}
              }
 
 puts 'Enter Dish Name'
-dish_name=gets.chomp
+dish_name = gets.chomp
 puts 'Enter Price'
-dish_price=gets.chomp.to_f
+dish_price = gets.chomp.to_f
 
-flg=0
+flg = 0
 
 for get_keys in hotel_menu.keys
-	unless hotel_menu[get_keys][dish_name]==nil
-		if hotel_menu[get_keys][dish_name] <= dish_price
-			if(flg==0)
-				puts "--------Hotels Available-----------"
-				flg=1
-			end
-			puts get_keys
-		end
-	end
+    unless hotel_menu[get_keys][dish_name] == nil
+        if hotel_menu[get_keys][dish_name] <= dish_price
+            if(flg == 0)
+                puts '--------Hotels Available-----------'
+                flg = 1
+            end
+            puts get_keys
+        end
+    end
 end
 
-if(flg==0)
-  puts "---------Sorry No Hotels Available----------"
-end
+puts '---------Sorry No Hotels Available----------' if flg == 0
